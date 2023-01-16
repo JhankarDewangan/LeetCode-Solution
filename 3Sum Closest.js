2a67d3af4b486bec52dd1b5b54d562a8e40e897b
Jhankar Dewangan <117924287+JhankarDewangan@users.noreply.github.com>
2023-01-17 00:33:21 +0530

    Solution of 3Sum Closest Problem

---
+++ 3Sum Closest.js
@@ -0,0 +1,39 @@
+// Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+// Return the sum of the three integers.
+
+// You may assume that each input would have exactly one solution.
+
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+  var result = nums[1]+nums[0]+nums[2] 
+  let i = 0
+  while(i < nums.length-1)
+  {
+      var start = i+1;
+      var end = nums.length - 1;
+      while(start < end){
+          var sum = nums[i] + nums[start] + nums[end];
+          if(target == sum)
+              return sum;
+          if(Math.abs(sum-target) < Math.abs(result - target)){
+              result = sum;
+          }
+          if(sum > target)
+              end -= 1;
+          else
+              start += 1;
+      }
+      i++
+  }
+  return result;
+};
