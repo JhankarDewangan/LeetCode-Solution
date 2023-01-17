d298d7dbb4622bd15fe67fa220c6c4d3047078e5
Jhankar Dewangan <117924287+JhankarDewangan@users.noreply.github.com>
2023-01-17 19:09:22 +0530

    Solution of Difference Between Element Sum .....

---
+++ Difference Between Element Sum and Digit Sum of an Array.js
@@ -0,0 +1,31 @@
+// You are given a positive integer array nums.
+
+// The element sum is the sum of all the elements in nums.
+// The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+// Return the absolute difference between the element sum and digit sum of nums.
+
+// Note that the absolute difference between two integers x and y is defined as |x - y|
+
+// Input: nums = [1,15,6,3]
+// Output: 9
+// Explanation: 
+// The element sum of nums is 1 + 15 + 6 + 3 = 25.
+// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var differenceOfSum = function(nums) {
+    let digit = nums.join('');
+    let digitCount = 0;
+    let count = 0;
+    for(let i = 0;i< nums.length;i++){
+        count += nums[i];
+    }
+    for(let j = 0;j < digit.length;j++){
+                digitCount += +digit[j];
+    }
+    return Math.abs(digitCount - count ) 
+};
