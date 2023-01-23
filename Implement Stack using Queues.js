42888ccc31030ee2280dd40f6f7d5ab674c475fd
Jhankar Dewangan <117924287+JhankarDewangan@users.noreply.github.com>
2023-01-23 16:10:33 +0530

    Solution of Implement Stack Using Queues Problem

---
+++ Implement Stack using Queues.js
@@ -0,0 +1,68 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+// Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+// Input
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 2, 2, false]
+
+// Explanation
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // return 2
+// myStack.pop(); // return 2
+// myStack.empty(); // return False
+
+var MyStack = function() {
+    this.arr  =[];   
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+     this.arr.unshift(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+        return this.arr.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.arr[0];
+
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+     return this.arr.length==0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
