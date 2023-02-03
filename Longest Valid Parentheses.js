1cf14fd62851b8e8b367834044c1d39a3f1cc3d2
Jhankar Dewangan <117924287+JhankarDewangan@users.noreply.github.com>
2023-02-03 20:52:40 +0530

    Solution of Longest Valid Parentheses Problem

---
+++ Longest Valid Parentheses.js
@@ -0,0 +1,37 @@
+// Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+// substring
+// .
+
+// Input: s = "(()"
+// Output: 2
+// Explanation: The longest valid parentheses substring is "()".
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    if(s.length === 0) return 0;
+   
+   let stack = [-1];
+   let max = 0;
+   
+   for(let i = 0; i < s.length; i++) {
+       //push into stack if opening paran
+       if(s[i] === '(') {
+           stack.push(i);
+       } else {
+           // pop out if it's an closing paran
+           stack.pop();
+           
+           // if we don't hav anything in stack, push this new index
+           if(stack.length === 0) {
+               stack.push(i);
+           } else {
+               // i - stack[stack.length - 1] is length of longest substr
+               max = Math.max(max, i - stack[stack.length - 1]);
+           }
+       }
+   }
+   return max;
+};
